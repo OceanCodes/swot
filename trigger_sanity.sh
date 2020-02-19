d4e5aa9ba3ec2baa938126aa5f0437560d774ce7
Segev Finer <segev@codeocean.com>
2020-02-19 16:52:43 +0200

    Migrate to CircleCI v2.1

--- trigger_sanity.sh
+++ trigger_sanity.sh
@@ -1,17 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 set -x
 
-# First, avoid redundant sanity builds by canceling existing builds
-BUILDS=`curl -sSLf https://circleci.com/api/v1.1/project/github/OceanCodes/sanity\?circle-token\=${CIRCLE_TOKEN} | jq/jq -c 'map(select(.status | contains("not_running") or contains("running"))) | .[].build_num'`
-
-if [ -n "$BUILDS" ]; then
-    while read -r build_num; do
-        curl -X POST https://circleci.com/api/v1.1/project/github/OceanCodes/sanity/${build_num}/cancel?circle-token=${CIRCLE_TOKEN}
-    done <<< "$BUILDS"
-fi
-
-curl \
-	--header "Content-Type: application/json" \
-	--data '{"build_parameters": {"TRIGGER": "ide"}}' \
-	--request POST \
-	https://circleci.com/api/v1/project/OceanCodes/sanity/tree/master?circle-token=$CIRCLE_TOKEN
+curl -X POST https://circleci.com/api/v2/project/gh/OceanCodes/sanity/pipeline?circle-token=$CIRCLE_TOKEN \
+	-H "Content-Type: application/json" \
+	--data-binary "{\"parameters\": {\"build_target\": \"$1\"}}"
